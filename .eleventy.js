0e7238ca4f79e5dc580a438032d3fc4684239cab
Sophie Beaumont <sophie.beaumont@bbc.co.uk>
2021-04-12 22:30:16 +0100

    Make css work  and update gitignore

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,11 @@
-module.exports = {
-  dir: {
-    input: "./src",
-    output: "./_site",
-  },
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('src/css')
+  return {
+    passthroughFileCopy: true,
+    dir: {
+      input: "./src",
+      output: "./_site",
+    },
+  }
 };
